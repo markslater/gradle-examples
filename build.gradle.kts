7c6db270504be7d185a22519e5f1b397ae42dac4
markslater <mark.slater@mail.com>
2023-04-25 10:51:04 +0100

    Follow advice from https://discuss.gradle.org/t/setting-project-version-using-a-plugin/45475/2 to defer version calculation until it's required by having a dynamic toString(!).

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,23 @@
+object Dodgy {
+    override fun toString(): String {
+        println("Called toString")
+        return "qux"
+    }
+}
+
+open class VersionPluginExtension {
+    val version = Dodgy
+}
+
 class VersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.version = System.getenv("FOO") ?: "bar"
+        project.extensions.create<VersionPluginExtension>("versionPlugin")
     }
 }
 
 apply<VersionPlugin>()
+
+version = the<VersionPluginExtension>().version
 
 task("printVersion") {
     doLast {
