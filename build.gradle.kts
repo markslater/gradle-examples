9d89c8e750f71682f8b10cd7aea31d18a6bcc3aa
Mark Slater <mark.slater@mail.com>
2024-04-19 12:49:45 +0100

    Upgrade gradle to 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     kotlin("jvm") version "1.8.20"
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter")
